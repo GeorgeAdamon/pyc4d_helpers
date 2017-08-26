5b2ec78d9b3e7bf6d283da02f66147e401608a0e
GeorgeAdamon <ga.adamon@gmail.com>
2017-08-26 22:56:01 +0100

    Various Fixes

--- scripts/UserDataHelpers.py
+++ scripts/UserDataHelpers.py
@@ -6,7 +6,7 @@ def UserDataExists(obj, itemName):
     
     Args:
         obj (c4d.BaseObject): The Cinema4D object to examine
-        itemName (string): The name of the UserData to search for
+        itemName (str): The name of the UserData to search for
     
     Returns:
         True if an item with the specified name exists,
@@ -22,6 +22,7 @@ def UserDataExists(obj, itemName):
                 return True
     return False
 
+
 def FindUserData (obj, itemName):
 
     """ 
@@ -29,7 +30,7 @@ def FindUserData (obj, itemName):
     
     Args:
         obj (c4d.BaseObject): The Cinema4D object to examine
-        itemName (string): The name of the UserData to search for
+        itemName (str): The name of the UserData to search for
     
     Returns:
         If an item with the specified name exists, the function returns the value/object contained in that time. 
@@ -47,14 +48,31 @@ def FindUserData (obj, itemName):
     else:
         return None
 
-def CreateUserData (obj, itemName, itemtype, value=None):
+
+def DestroyUserData(obj, itemName):
+
+    if obj==None: return False
+    UserData = obj.GetUserDataContainer()
+    
+    if UserData:
+        for ud in UserData:
+            if ud[1][c4d.DESC_NAME] == itemName:
+                obj.RemoveUserData(ud[0]) #The actual object contained in the UserData item
+                c4d.EventAdd()
+
+                return True
+                
+    return False
+
+
+def CreateUserData (obj, itemName, itemtype, overwrite=False):
 
     """ 
     Create a new UserData item and add it to the UserDataContainer of the specified object.
 
     Args:
         obj (c4d.BaseObject): The Cinema4D object to add UserData to.
-        itemName (string): The name of the UserData item to be created.
+        itemName (str): The name of the UserData item to be created.
         itemtype: The data type of the UserData item to be created. Available DataTypes:
             c4d.DTYPE_BASELISTLINK  (BaseList2D).
             c4d.DTYPE_BOOL  (bool).
@@ -78,19 +96,56 @@ def CreateUserData (obj, itemName, itemtype, value=None):
             c4d.DTYPE_TEXTURE   (Texturename).
             c4d.DTYPE_TIME  (BaseTime).
             c4d.DTYPE_VECTOR    (Vector).
-        value: Optional value of the UserData item
+        overwrite: Whether to overwrite any existing UserData with same name. False by default
     Returns:
         True on success, False on failure
      """
     if obj==None: return False
     if itemtype==None: itemtype = c4d.NONE
-    if itemName == None: itemName = "No Name"
+    
+    UserData = obj.GetUserDataContainer()
+    if itemName == None: itemName = "No Name " + str(len(UserData))
 
+    if UserDataExists(obj,itemName):
+        if overwrite==False: 
+            return False
+        else:
+            DestroyUserData(obj,itemName)
+
+    BaseContainer = c4d.GetCustomDatatypeDefault(itemtype)
     BaseContainer[c4d.DESC_NAME] = itemName
-    BaseContainer = c4d.GetCustomDataTypeDefault(itemtype)
+    BaseContainer[c4d.DESC_SHORT_NAME] = itemName
+
+    if itemtype==c4d.DTYPE_BUTTON:
+        BaseContainer[c4d.DESC_CUSTOMGUI] =  c4d.CUSTOMGUI_BUTTON
 
     item = obj.AddUserData(BaseContainer)
-    obj[item] = value
     c4d.EventAdd()
 
     return True
+
+
+def SetUserData (obj, itemName, value):
+    """ 
+    Sets the value of a UserData item of a Cinema4D object
+
+    Args:
+        obj (c4d.BaseObject): The Cinema4D object whose data to alter.
+        itemName(str): The name of the UserData item to be altered.
+        value: The value or object to set. 
+    Returns:
+        True on success,
+        False on failure
+    """
+    if obj==None: return False
+    
+    UserData = obj.GetUserDataContainer()
+    
+    if UserData:
+        for ud in UserData:
+            if ud[1][c4d.DESC_NAME] == itemName:
+                obj[ud[0]] = value
+                c4d.EventAdd()
+                return True
+    return False
+

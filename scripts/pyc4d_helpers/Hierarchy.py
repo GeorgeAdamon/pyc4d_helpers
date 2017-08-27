7d3476fd6976ab55ce92d2d6fb1b6f2fec2baf56
GeorgeAdamon <ga.adamon@gmail.com>
2017-08-27 14:34:10 +0100

    Tab fixes

--- scripts/pyc4d_helpers/Hierarchy.py
+++ scripts/pyc4d_helpers/Hierarchy.py
@@ -1,30 +1,35 @@
 import c4d
 
 def SelectObject(obj):
-	obj.SetBit(c4d.BIT_ACTIVE)
-	return
+    obj.SetBit(c4d.BIT_ACTIVE)
+    return
 
 def DeselectObject(obj):
-	obj.DelBit(c4d.BIT_ACTIVE)
-	return
+    obj.DelBit(c4d.BIT_ACTIVE)
+    return
 
 def SelectChildren(obj, recursive = True, deselect_parent = True):
-	if recursive:
-		doc.StartUndo()
-    	SelectChildrenHelper(obj,obj.GetNext())
+    if recursive:
+        doc.StartUndo()
+        SelectChildren_RecursionHelper(obj,obj.GetNext())
 
-    	if deselect_parent:
-    		DeselectObject(obj)
+        if deselect_parent:
+            DeselectObject(obj)
+            doc.EndUndo()
+    else:
+        doc.StartUndo()
+        SelectChildren_Helper(obj,obj.GetNext())
 
-   		doc.EndUndo()
-   	else:
+        if deselect_parent:
+            DeselectObject(obj)
 
+        doc.EndUndo()
+    return
 
 def SelectChildren_RecursionHelper(obj, next):
-	
-	while obj and obj != next: #while we are not in the end of this object's hierarchy
+    while obj and obj != next: #while we are not in the end of this object's hierarchy
 
-        doc.AddUndo(c4d.UNDOTYPE_CHANGE_SMALL,obj)
+        doc.AddUndo(c4d.UNDOTYPE_CHANGE_SMALL,obj)  
 
         SelectObject(obj) #select the current object
 
@@ -37,16 +42,15 @@ def SelectChildren_RecursionHelper(obj, next):
     return True
 
 def SelectChildren_Helper(obj, next):
-	
-	while obj and obj != next: #while we are not in the end of this object's hierarchy
+    obj2 = obj.GetDown()
 
-        doc.AddUndo(c4d.UNDOTYPE_CHANGE_SMALL,obj)
+    while obj2 and obj2 != next: #while we are not in the end of this object's hierarchy
 
-        SelectObject(obj) #select the current object
+        doc.AddUndo(c4d.UNDOTYPE_CHANGE_SMALL,obj2)
 
-        SelectChildrenHelper(obj.GetDown(),next)  #Call this same function on the first child of the current object, if it exists. This initiates a recursive chain reaction, until we reach the end of this object's hierarchy
+        SelectObject(obj2) #select the current object
 
-        obj = obj.GetNext()  #Get the next object in the hierarchy
+        obj2 = obj2.GetNext()  #Get the next object in the hierarchy
 
     c4d.EventAdd()
 
